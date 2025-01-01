57cd7640ec7f8f6cbd348091838e63bf5a00a93e
Nishant Chhillar <115921678+nchhillar2004@users.noreply.github.com>
2025-01-01 19:19:58 +0530

    1422. Maximum Score After Splitting a String 1/1/2025

---
+++ easy/1422. Maximum Score After Splitting a String/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxScore(String s) {
+        int totalOnes = 0;
+        for (char ch : s.toCharArray()){
+            if (ch == '1') totalOnes++;
+        }
+
+        int leftZeros = 0;
+        int rightOnes = totalOnes;
+        int maxScore = 0;
+
+        for (int i = 0; i < s.length()-1; i++){
+            if (s.charAt(i) == '0'){
+                leftZeros++;
+            }else{
+                rightOnes--;
+            }
+
+            maxScore = Math.max(maxScore, leftZeros + rightOnes);
+        }
+
+        return maxScore;
+    }
+}
