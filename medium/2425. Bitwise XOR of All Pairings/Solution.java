986537b67837bc93ec18425df06527ad7b11b76c
Nishant Chhillar <115921678+nchhillar2004@users.noreply.github.com>
2025-01-16 10:16:21 +0530

    2425 16/1/2025

---
+++ medium/2425. Bitwise XOR of All Pairings/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int x1 = 0, x2 = 0;
+
+        if (nums1.length % 2 != 0){
+            for (int num : nums2) x2 ^= num;
+        }
+
+        if (nums2.length % 2 != 0){
+            for (int num : nums1) x1 ^= num;
+        }
+
+        return x1 ^= x2;
+    }
+}
